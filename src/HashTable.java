4b4e093a01a3ef8ad570e342f3bccc5a070e9c8c
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2020-07-13 23:59:31 +0800

    HashTable resize方法

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,6 +1,9 @@
 import java.util.TreeMap;
 
 public class HashTable<K, V> {
+    private static final int upperTol = 10;
+    private static final int lowerTol = 2;
+    private static final int initCapacity = 7;
     private TreeMap<K, V>[] hashTable;
     private int M;
     private int size;
@@ -15,7 +18,7 @@ public class HashTable<K, V> {
     }
 
     public HashTable() {
-        this(97);
+        this(initCapacity);
     }
 
     private int hash(K key) {
@@ -33,6 +36,10 @@ public class HashTable<K, V> {
         }else {
             map.put(key, value);
             size ++;
+
+            if (size >= upperTol * M) {
+                resize(2 * M);
+            }
         }
     }
 
@@ -42,6 +49,10 @@ public class HashTable<K, V> {
         if (map.containsKey(key)) {
             ret = map.remove(key);
             size --;
+
+            if (size < lowerTol * M && M / 2 > initCapacity) {
+                resize(M / 2);
+            }
         }
         return ret;
     }
@@ -60,6 +71,22 @@ public class HashTable<K, V> {
 
     public V get(K key) {
         return hashTable[hash(key)].get(key);
+    }
+
+    private void resize(int newM) {
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        for (int i = 0; i < newM; i++) {
+            newHashTable[i] = new TreeMap<>();
+        }
+        int oldM = M;
+        this.M = newM;
+        for (int i = 0; i < M; i++) {
+            TreeMap<K, V> map = hashTable[i];
+            for (K key : map.keySet()) {
+                newHashTable[hash(key)].put(key, map.get(key));
+            }
+        }
+        this.hashTable = newHashTable;
     }
 
 }
